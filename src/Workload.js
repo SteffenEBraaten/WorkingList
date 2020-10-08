9cb80ee7c0c72fff356fb8599a4c5719fc23faff
Tine Margretha Vister <tinemv@student.matnat.uio.no>
2020-10-08 11:37:30 +0200

    Finished displaying index cases

--- src/Workload.js
+++ src/Workload.js
@@ -3,6 +3,8 @@ import { useDataQuery } from "@dhis2/app-runtime";
 import { Menu, MenuItem, Card } from "@dhis2/ui";
 import styles from "./App.module.css";
 import WorkloadHeader from "./WorkloadHeader.js";
+import { ContentWriter } from "istanbul-lib-report";
+import { list } from "postcss";
 
 /*
 This file is for the 'main' page that contains list element 
@@ -22,15 +24,8 @@ const query = {
   },
 };
 
-const fakeData = {
-  cases: [
-    {
-      firstName: "Tine Margretha Vister",
-    },
-    {
-      firstName: "Steffen Ekeberg Bråten",
-    },
-  ],
+const indexCases = {
+  cases: [ ],
   contacts: [
     {
       contactNum: "1/2",
@@ -41,15 +36,49 @@ const fakeData = {
   ],
 };
 
-const Workload = () => {
-  /*   const { error, loading, data } = useDataQuery(query);
-   */
-  /* if (error) {
+const loadIndexCases = () => {
+  const { error, loading, data } = useDataQuery(query);
+
+  if (error) {
     return <p>Error</p>;
   }
   if (loading) {
     return <p>Loading</p>;
-  } */
+  }
+
+  data.allIndexCases.trackedEntityInstances.map(({ attributes }) => {
+    var tempNameList = []
+
+    if (attributes.find(item => item.code === "surname") !== undefined){
+      tempNameList.push({
+        key: "lastName",
+        value: attributes.find(item => item.code === "surname").value
+      })
+    }
+
+    if (attributes.find(item => item.code === "first_name") !== undefined){
+      tempNameList.push({
+        key: "firstName",
+        value: attributes.find(item => item.code === "first_name").value
+      })
+    }
+
+    if (tempNameList.length !== 0) indexCases["cases"].push(tempNameList)
+  });
+}
+
+const getName = () => {
+  var nameList = []
+  indexCases.cases.map((x) => {
+    if (x.length > 1) nameList.push(x[1].value.concat(" ", x[0].value))
+    else nameList.push(x[0].value)
+  })
+
+  return nameList
+} 
+
+const Workload = () => {
+  loadIndexCases()
 
   return (
     <>
@@ -57,19 +86,20 @@ const Workload = () => {
         <div class="">
           <Menu>
             <h3>Index cases</h3>
-            {fakeData.cases.map((x) => (
-              <MenuItem
-                dataTest="dhis2-uicore-card"
-                label={x.firstName}
-                className={styles.listItemCaseWorkload}
-              ></MenuItem>
-            ))}
+            {getName().map((x) => (
+                <MenuItem
+                  dataTest="dhis2-uicore-card"
+                  label={x}
+                  className={styles.listItemCaseWorkload}
+                ></MenuItem>
+              ))
+            }
           </Menu>
         </div>
         <div class="">
           <Menu>
             <h3 className={styles.h3Center}>Contacts</h3>
-            {fakeData.contacts.map((x) => (
+            {indexCases.contacts.map((x) => (
               <MenuItem
                 dataTest="dhis2-uicore-card"
                 label={x.contactNum}
