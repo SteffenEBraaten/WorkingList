6a7590abca60bcee827f67b2c8814b46b533251d
ThanhThaoThiTran <thanht@uio.no>
2020-10-20 22:03:00 +0200

    #7 første iterasjon ferdig, trenger bedre ekspertise til å se på layout

--- src/components/Workload/WorkloadTable.jsx
+++ src/components/Workload/WorkloadTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Table,
   TableHead,
@@ -11,6 +11,7 @@ import {
 } from "@dhis2/ui";
 import trackerCaptureURL from "../../api/Urls";
 import { findValue } from "../../api/APIUtils";
+import ContactsModal from "./ContactsModal.jsx";
 
 const toDateAndTimeFormat = (dateString, time = true) => {
   const date = time
@@ -41,56 +42,103 @@ const goToTrackerCaptureApp = (trackedEntityInstance, programID, orgUnit) => {
 };
 
 const WorkloadTable = ({ data }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalObject, setObject] = useState({});
+  const showContactsModal = (
+    program,
+    first_name,
+    surname,
+    relationshipEntityInstance,
+    hideModal
+  ) => {
+    let entityInstance = relationshipEntityInstance;
+    if (program == "Index case") {
+      if (relationshipEntityInstance.length != 0) {
+        entityInstance =
+          relationshipEntityInstance[0].to.trackedEntityInstance
+            .trackedEntityInstance;
+      }
+
+      setObject({
+        first_name: first_name,
+        surname: surname,
+        relationshipEntityInstance: entityInstance,
+        hideModal: hideModal,
+      });
+      setShowModal(true);
+    }
+  };
+
   return (
-    <Table>
-      <TableHead>
-        <TableRowHead>
-          <TableCellHead>Type</TableCellHead>
-          <TableCellHead>First Name</TableCellHead>
-          <TableCellHead>Last Name</TableCellHead>
-          <TableCellHead>Phone Number</TableCellHead>
-          <TableCellHead>Age</TableCellHead>
-          <TableCellHead>Incident Date</TableCellHead>
-          <TableCellHead>Last updated</TableCellHead>
-          <TableCellHead>Status</TableCellHead>
-          <TableCellHead>Link to Tracker Capture App</TableCellHead>
-        </TableRowHead>
-      </TableHead>
-      <TableBody>
-        {data.map((item, key) => (
-          <TableRow key={key}>
-            <TableCell>
-              {mapProgramIDToName(item.enrollments[0].program)}
-            </TableCell>
-            <TableCell>{findValue(item.attributes, "first_name")}</TableCell>
-            <TableCell>{findValue(item.attributes, "surname")}</TableCell>
-            <TableCell>{findValue(item.attributes, "phone_local")}</TableCell>
-            <TableCell>
-              {findValue(item.attributes, "patinfo_ageonset")}
-            </TableCell>
-            <TableCell>
-              {toDateAndTimeFormat(item.enrollments[0].incidentDate, false)}
-            </TableCell>
-            <TableCell>{toDateAndTimeFormat(item.lastUpdated)}</TableCell>
-            <TableCell>{item.enrollments[0].status}</TableCell>
-            <TableCell>
-              <Button
-                primary
-                onClick={() =>
-                  goToTrackerCaptureApp(
-                    item.trackedEntityInstance,
-                    item.enrollments[0].program,
-                    item.orgUnit
-                  )
-                }
-              >
-                Tracker Capture App
-              </Button>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      <Table>
+        <TableHead>
+          <TableRowHead>
+            <TableCellHead>Type</TableCellHead>
+            <TableCellHead>First Name</TableCellHead>
+            <TableCellHead>Last Name</TableCellHead>
+            <TableCellHead>Phone Number</TableCellHead>
+            <TableCellHead>Age</TableCellHead>
+            <TableCellHead>Incident Date</TableCellHead>
+            <TableCellHead>Last updated</TableCellHead>
+            <TableCellHead>Status</TableCellHead>
+            <TableCellHead>Contacts</TableCellHead>
+            <TableCellHead>Link to Tracker Capture App</TableCellHead>
+          </TableRowHead>
+        </TableHead>
+        <TableBody>
+          {data.map((item, key) => (
+            <TableRow key={key}>
+              <TableCell>
+                {mapProgramIDToName(item.enrollments[0].program)}
+              </TableCell>
+              <TableCell>{findValue(item.attributes, "first_name")}</TableCell>
+              <TableCell>{findValue(item.attributes, "surname")}</TableCell>
+              <TableCell>{findValue(item.attributes, "phone_local")}</TableCell>
+              <TableCell>
+                {findValue(item.attributes, "patinfo_ageonset")}
+              </TableCell>
+              <TableCell>
+                {toDateAndTimeFormat(item.enrollments[0].incidentDate, false)}
+              </TableCell>
+              <TableCell>{toDateAndTimeFormat(item.lastUpdated)}</TableCell>
+              <TableCell>{item.enrollments[0].status}</TableCell>
+              <TableCell>
+                <Button
+                  primary
+                  onClick={() =>
+                    showContactsModal(
+                      mapProgramIDToName(item.enrollments[0].program),
+                      findValue(item.attributes, "first_name"),
+                      findValue(item.attributes, "surname"),
+                      item.relationships,
+                      setShowModal
+                    )
+                  }
+                >
+                  See contacts
+                </Button>
+              </TableCell>
+              <TableCell>
+                <Button
+                  primary
+                  onClick={() =>
+                    goToTrackerCaptureApp(
+                      item.trackedEntityInstance,
+                      item.enrollments[0].program,
+                      item.orgUnit
+                    )
+                  }
+                >
+                  Go to Tracker Capture App!
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {showModal && <ContactsModal item={modalObject} />}
+    </>
   );
 };
 
