7303feee4103bdd789d8cf01f489b7f3dcb32209
Wahed Mangal <mangal.wahed@gmail.com>
2020-10-27 17:39:29 +0100

    fix: wraps parent setState call inside a useEffect callback

--- src/components/Workload/Workload.jsx
+++ src/components/Workload/Workload.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { CircularLoader, NoticeBox } from "@dhis2/ui";
-import styles from "./Workload.module.css";
-import { useDataQuery } from "@dhis2/app-runtime";
-import { CaseEnum, DueDateEnum } from "../Enum/Enum";
-import { WorkloadTable, toDateAndTimeFormat } from "./WorkloadTable";
-import SearchComponent from "./SearchComponent";
-import { findValue } from "../../api/APIUtils";
+import React, { useEffect, useState } from "react"
+import { CircularLoader, NoticeBox } from "@dhis2/ui"
+import styles from "./Workload.module.css"
+import { useDataQuery } from "@dhis2/app-runtime"
+import { CaseEnum, DueDateEnum } from "../Enum/Enum"
+import { WorkloadTable, toDateAndTimeFormat } from "./WorkloadTable"
+import SearchComponent from "./SearchComponent"
+import { findValue } from "../../api/APIUtils"
 
 const Workload = (props) => {
-  const filtered = props.indexFilterSelected;
-  const caseStatus = props.statusSelected;
-  const datesSelected = props.datesSelected;
+  const filtered = props.indexFilterSelected
+  const caseStatus = props.statusSelected
+  const datesSelected = props.datesSelected
   const [searchValue, setSearchValue] = useState("")
 
   const option = {
     variables: {
       programStatus: caseStatus,
     },
-  };
+  }
 
   const queryContact = {
     contacts: {
@@ -42,7 +42,7 @@ const Workload = (props) => {
         paging: false,
       }),
     },
-  };
+  }
 
   const queryIndex = {
     indexCases: {
@@ -66,39 +66,136 @@ const Workload = (props) => {
         paging: false,
       }),
     },
-  };
+  }
 
   const {
     error: indexCaseError,
     loading: indexCaseLoading,
     data: indexCasesData,
     refetch: indexcaseRefetch,
-  } = useDataQuery(queryIndex, option);
+  } = useDataQuery(queryIndex, option)
 
   const {
     error: contactCaseError,
     loading: contactCaseLoading,
     data: contactCasesData,
     refetch: contactCaseRefetch,
-  } = useDataQuery(queryContact, option);
+  } = useDataQuery(queryContact, option)
 
   useEffect(() => {
     async function fetchIndex() {
-      await indexcaseRefetch(option.variables);
+      await indexcaseRefetch(option.variables)
     }
     async function fetchContact() {
-      await contactCaseRefetch(option.variables);
+      await contactCaseRefetch(option.variables)
     }
 
     if (filtered === CaseEnum.ALL) {
-      fetchIndex();
-      fetchContact();
-    } else if (filtered === CaseEnum.INDEXES) fetchIndex();
-    else fetchContact();
-  }, [filtered, caseStatus]);
+      fetchIndex()
+      fetchContact()
+    } else if (filtered === CaseEnum.INDEXES) fetchIndex()
+    else fetchContact()
+  }, [filtered, caseStatus])
+
+  const hasData = indexCasesData && contactCasesData
+
+  const both =
+    hasData &&
+    indexCasesData.indexCases.trackedEntityInstances.concat(
+      contactCasesData.contacts.trackedEntityInstances
+    )
+
+  let dataToDisplay = hasData
+    ? filtered === CaseEnum.ALL
+      ? both
+      : filtered === CaseEnum.INDEXES
+      ? indexCasesData.indexCases.trackedEntityInstances
+      : contactCasesData.contacts.trackedEntityInstances
+    : []
+
+  // filter data on selected date
+  const filterData = (dataToDisplay) => {
+    const newDataToDisplay = []
+
+    const from = datesSelected.from
+    const fromDate = new Date(`${from.year}`, `${from.month}`, `${from.day}`)
+
+    const to = datesSelected.to
+    const toDate =
+      to === null
+        ? fromDate
+        : new Date(`${to.year}`, `${to.month}`, `${to.day}`)
+
+    // loop through data
+    for (let i = 0; dataToDisplay && i < dataToDisplay.length; i++) {
+      // loop through events
+      for (let j = 0; j < dataToDisplay[i].enrollments[0].events.length; j++) {
+        const event = dataToDisplay[i].enrollments[0].events[j]
+        const dueDateList = toDateAndTimeFormat(event.dueDate, false).split(".")
+        const dueDate = new Date(dueDateList[2], dueDateList[1], dueDateList[0]) // formate Date object to prepare for comparing
+
+        if (
+          event.status === DueDateEnum.SCHEDULE &&
+          dueDate >= fromDate &&
+          dueDate <= toDate
+        ) {
+          // filter on search bar
+          if (searchValue !== "") {
+            const firstName = findValue(
+              dataToDisplay[i].attributes,
+              "first_name"
+            ).toLowerCase()
+            const lastName = findValue(
+              dataToDisplay[i].attributes,
+              "surname"
+            ).toLowerCase()
+            const fullName = firstName.concat(" ", lastName)
+
+            if (fullName.includes(searchValue)) {
+              newDataToDisplay.push(dataToDisplay[i])
+            }
+          }
+
+          // if not user search, view full list
+          else {
+            newDataToDisplay.push(dataToDisplay[i])
+          }
+          break
+        }
+      }
+    }
+    return newDataToDisplay
+  }
+
+  dataToDisplay = filterData(dataToDisplay)
+
+  const programDictonary = {
+    uYjxkTbwRNf: "Index case",
+    DM9n1bUw8W8: "Contact",
+  }
+
+  const mapProgramIDToName = (programID) => {
+    const name = programDictonary[programID]
+      ? programDictonary[programID]
+      : programID
+    return name
+  }
+
+  const isIndexCase = (tei) =>
+    mapProgramIDToName(tei.enrollments[0].program) === "Index case"
+
+  let teller = 0
+  for (let i = 0; i < dataToDisplay.length; i++) {
+    if (isIndexCase(dataToDisplay[i])) teller++
+  }
+
+  useEffect(() => {
+    props.setNumberOfCases(dataToDisplay.length)
+    props.setNumberOfIndexCases(teller)
+  }, [dataToDisplay.length, teller])
 
   if (indexCaseLoading || contactCaseLoading) {
-    return <CircularLoader className={styles.centerElement} />;
+    return <CircularLoader className={styles.centerElement} />
   }
 
   if (indexCaseError || contactCaseError) {
@@ -110,99 +207,15 @@ const Workload = (props) => {
       >
         Could not get the working list. Please try again later.
       </NoticeBox>
-    );
+    )
   }
-
-  const both = indexCasesData.indexCases.trackedEntityInstances.concat(
-    contactCasesData.contacts.trackedEntityInstances
-  );
-
-  let dataToDisplay =
-    filtered === CaseEnum.ALL
-      ? both
-      : filtered === CaseEnum.INDEXES
-      ? indexCasesData.indexCases.trackedEntityInstances
-      : contactCasesData.contacts.trackedEntityInstances;
-
-  // filter data on selected date
-  const filterData = (dataToDisplay) => {
-    const newDataToDisplay = [];
-
-    const from = datesSelected.from;
-    const fromDate = new Date(`${from.year}`, `${from.month}`, `${from.day}`);
-
-    const to = datesSelected.to;
-    const toDate = to === null ? fromDate : new Date(`${to.year}`, `${to.month}`, `${to.day}`);
-
-    // loop through data
-    for (var i = 0; i < dataToDisplay.length; i++) {
-      // loop through events
-      for (var j = 0; j < dataToDisplay[i].enrollments[0].events.length; j++) {
-        const event = dataToDisplay[i].enrollments[0].events[j];
-        const dueDateList = toDateAndTimeFormat(event.dueDate, false).split(
-          "."
-        );
-        const dueDate = new Date(
-          dueDateList[2],
-          dueDateList[1],
-          dueDateList[0]
-        ); // formate Date object to prepare for comparing
-
-        if (event.status === DueDateEnum.SCHEDULE && (dueDate >= fromDate && dueDate <= toDate)){
-          // filter on search bar
-          if (searchValue !== "") {
-            const firstName = findValue(dataToDisplay[i].attributes, "first_name").toLowerCase()
-            const lastName = findValue(dataToDisplay[i].attributes, "surname").toLowerCase()
-            const fullName = firstName.concat(" ", lastName)
-
-            if (fullName.includes(searchValue)){
-              newDataToDisplay.push(dataToDisplay[i])
-            }
-          }
-
-          // if not user search, view full list
-          else {
-            newDataToDisplay.push(dataToDisplay[i])
-          }
-          break;
-        }
-      }
-    }
-    return newDataToDisplay;
-  };
-
-  dataToDisplay = filterData(dataToDisplay);
-  props.setNumberOfCases(dataToDisplay.length);
-
-  const programDictonary = {
-    uYjxkTbwRNf: "Index case",
-    DM9n1bUw8W8: "Contact"
-  };
-
-  const mapProgramIDToName = programID => {
-    const name = programDictonary[programID]
-      ? programDictonary[programID]
-      : programID;
-    return name;
-  };
-
-  const isIndexCase = (tei) =>
-  mapProgramIDToName(tei.enrollments[0].program) === "Index case";
-
-
-  let teller = 0;
-  for (let i = 0; i < dataToDisplay.length; i++) {
-    if(isIndexCase(dataToDisplay[i])) teller++;
-  }
-
-  props.setNumberOfIndexCases(teller);
 
   return (
     <div className={styles.workloadContainer}>
-      <SearchComponent setSearchValue={setSearchValue}/>
-      <WorkloadTable data={dataToDisplay} filter={filtered}/>
+      <SearchComponent setSearchValue={setSearchValue} />
+      <WorkloadTable data={dataToDisplay} filter={filtered} />
     </div>
-  );
-};
+  )
+}
 
-export default Workload;
+export default Workload
