f1416e14071bc8807034017b0fe0992172ff3fef
Tine Margretha Vister <tinemv@student.matnat.uio.no>
2020-10-28 10:26:39 +0100

    Fixed functionality, design next

---
+++ src/components/Workload/components/WorkloadTable/DropDownStatus.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { StatusEnum } from "../../../Enum/Enum";
+import {
+  isOverdue,
+  toDateAndTimeFormat,
+  mapProgramStageIdToName
+} from "../../../../utils/APIUtils";
+import { Tag, DropdownButton, Card } from "@dhis2/ui";
+import styles from "./WorkloadTable.module.css";
+const eventTagMapper = (eventStatus, eventDueDate) => {
+    if (
+      (eventStatus === StatusEnum.SCHEDULE && isOverdue(eventDueDate)) ||
+      eventStatus === StatusEnum.OVERDUE
+    ) {
+      return { negative: true };
+    }
+    if (eventStatus === StatusEnum.SCHEDULE) {
+      return { neutral: true };
+    }
+    if (eventStatus === StatusEnum.COMPLETED) {
+      return { positive: true };
+    } else return {};
+  };
+const DropDownStatus = ({events}) => {
+    const lastEvent = events.pop();
+    return (
+            <DropdownButton
+            className={styles.dropDownStatus}
+            component={
+                <Card>
+                {events.map((thisEvent, key) => (
+                <div key={key} className={styles.statusTagContainer}>
+                <Tag className={styles.tag}
+                    {...eventTagMapper(thisEvent.status, thisEvent.dueDate)}>
+                    {`${toDateAndTimeFormat(
+                        thisEvent.dueDate,
+                        false
+                        )} ${mapProgramStageIdToName(thisEvent.programStage)} ${
+                    isOverdue(thisEvent.dueDate) &&
+                    thisEvent.status === StatusEnum.SCHEDULE
+                        ? StatusEnum.OVERDUE
+                        : thisEvent.status
+                    }`}
+                </Tag>
+                </div>
+                ))}
+            </Card>}
+            dataTest="dhis2-uicore-dropdownbutton"
+            name="default"
+            value="nothing">
+                <Tag {...eventTagMapper(lastEvent.status, lastEvent.dueDate)}>
+                    {`${toDateAndTimeFormat(
+                        lastEvent.dueDate,
+                        false
+                        )} ${mapProgramStageIdToName(lastEvent.programStage)} ${
+                    isOverdue(lastEvent.dueDate) &&
+                    lastEvent.status === StatusEnum.SCHEDULE
+                        ? StatusEnum.OVERDUE
+                        : lastEvent.status
+                    }`}
+                </Tag>
+            </DropdownButton>
+    )
+}
+export default DropDownStatus;
